0d53f5bcea1333bd988ebd8f304f85e0fa74f65f
Guy Bitan <gb824v@intl.att.com>
2018-05-24 14:15:28 +0300

    Commit

--- src/biq/java8/exam/Lambda/ForeachConsumer/MainForEachConsumer.java
+++ src/biq/java8/exam/Lambda/ForeachConsumer/MainForEachConsumer.java
@@ -3,7 +3,7 @@ package biq.java8.exam.Lambda.ForeachConsumer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ForEachMain {
+public class MainForEachConsumer {
 
     public static void main(String[] args) {
         List<String> myString = new ArrayList<>();
@@ -29,16 +29,15 @@ public class ForEachMain {
         myString.forEach(str -> System.out.println(str));
 
 		/*
-		MethodReferences
+        forEach(Consumer<T>):
+        Consumer<T> = Accept a single input argument of type T, and return no result
+        In this case get string and no results only print;
+
+        MethodReferences:
         Method references let you reuse a method as a lambda expression.
         It’s a way to pass a method name without calling it.
         Use a method reference is done using the following syntax:
         <Where the method is found> :: <method name>
-
-        forEach(Consumer<T>)
-        Consumer<T> = Accept a single input argument of type T, and return no result
-        In this case get string and no results only print;
-
 		*/
 
         myString.forEach(System.out::println); //=> myString.forEach(str -> System.out.println(str))
--- src/biq/java8/exam/MethodReferences/SuperSpecialPersonComparison.java
+++ src/biq/java8/exam/MethodReferences/SuperSpecialPersonComparison.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package biq.java8.exam.MethodReferences;
-
 import biq.java8.exam.Lambda.Predicate.model.Person;
 
-/**
- * @author blechl
- */
 public class SuperSpecialPersonComparison {
 
     public int compareByAge(Person p1, Person p2) {
